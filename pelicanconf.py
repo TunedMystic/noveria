6505df2fd8009d3f08a84cc7f9d86bfe7f0e3533
= <=>
2015-02-18 21:47:19 -0500

    Blog setup.

---
+++ pelicanconf.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Sandeep Jadoonanan'
+SITENAME = u'Smallcode Blog'
+ALT_NAME = u"> .logs"
+SITEURL = ''
+SITESUBTITLE = u"The web is awesome."
+DESCRIPTION = u"Programming snippets, Python code, Random thoughts."
+
+PATH = 'content'
+STATIC_PATHS = ["img", "pdf", "pages", "feeds"]
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+THEME = "themes/mg"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Linkedin', 'http://linkedin.com/in/sandeepjadoonanan'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
+
+# Social widget
+SOCIAL = (('twitter', 'https://twitter.com/sandcoder'),
+		    ('google-plus-square', 'https://plus.google.com/+SmallcodeMe'),
+		    ('github', 'https://github.com/tunedmystic'),
+		    ("linkedin-square", "https://linkedin.com/in/sandeepjadoonanan"),
+		    ('info-circle', 'http://smallcode.me'),)
+
+
+DEFAULT_PAGINATION = 5
+
+# Set to True if you want document-relative URLs when developing
+RELATIVE_URLS = False
+
+FOOTER = ("&copy; Sandeep Jadoonanan.<br>" +
+           "<em style=\"color: #999;\">Technology is beautiful and dangerous.</em>")
+              
+# mg specific settings.
+SHARE = True
+TAG_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+DIRECT_TEMPLATES = ('index', 'categories', 'archives', 'search', 'tipue_search')
+TIPUE_SEARCH_SAVE_AS = 'tipue_search.json'
+#DISQUS_SITENAME = "smallcodeblog"
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+
